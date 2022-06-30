1f981893f1f438bd54474bc15e27cea86683f930
MouraRaquel <raquelaguiar@id.uff.br>
2022-06-30 07:41:20 -0300

    Insert Flags

--- comparativo/src/app/app.module.ts
+++ comparativo/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuemsouComponent } from './components/quemsou/quemsou.component';
